6c7414a970fe38e9e0da766392d16b0cbd2f34d0
Graham Arnett <GrahamKentArnett@outlook.com>
2024-11-06 15:41:59 -0500

    init commit

---
+++ Favorite color app/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Favorite_color_app
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Favorite Color App");
+            string choice;
+            do {
+                //, here's a flower in your favorite color, I want to have ascii flowers that get printed but not yet
+                Console.Write("\n\nWhat is your favorite of the primary subtractive colors? Red, Yellow, or Blue? ");
+                String favorite = Console.ReadLine().ToLower();
+                switch (favorite)
+                {
+                    case "yellow":
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.WriteLine("Yellow's my favorite too.");
+                        break;
+                    case "blue":
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                        Console.WriteLine("The color of the sky? Nice choice.");
+                        break;
+                    case "red":
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("The color of fire? Nice choice.");
+                        break;
+                }
+                Console.Write("Would you like to go again? (y/n): ");
+                choice = Console.ReadLine().ToLower();
+            }while(choice == "y");
+            Console.WriteLine("Goodbye");
+        }
+    }
+}
